bdd4e9eb05c9447370678a6eec824b64a3190c75
Jonatan <jonatanborba1987@gmail.com>
2026-05-03 11:33:34 -0300

    agregue .env

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,17 +1,24 @@
 import { Module } from '@nestjs/common';
 import {TypeOrmModule} from '@nestjs/typeorm';
 import { AuthModule } from './modules/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: 'maraluna2026',
-  database: 'gestion_proyectos',
-  autoLoadEntities: true,
-  synchronize: false,
+    imports: 
+  
+  [ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+    
+    type: 'postgres',
+    host: process.env.DB_HOST,
+    port: Number(process.env.DB_PORT),
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    autoLoadEntities: true,
+    synchronize: false,
   }), AuthModule],
   controllers: [],
   providers: [],
