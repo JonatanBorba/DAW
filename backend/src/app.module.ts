66643bcdc5eb6a03cbb5b2b11b8db763ec492e64
Jonatan <jonatanborba1987@gmail.com>
2026-05-03 10:26:35 -0300

    estructua creada

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {TypeOrmModule} from '@nestjs/typeorm';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TypeOrmModule.forRoot({
+  type: 'postgres',
+  host: 'localhost',
+  port: 5432,
+  username: 'postgres',
+  password: 'maraluna2026',
+  database: 'gestion_proyectos',
+  autoLoadEntities: true,
+  synchronize: false,
+  }), AuthModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
