5eef53f1eb93c9cef9edb4fb00ecb011e4d299e8
Yamila Aguilar <aguilaryamila995@gmail.com>
2026-06-12 22:01:26 -0300

    Agregue el boton que va de gestion proyectos a tareas y corregi el CROS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:4200', 'https://localhost'],
+    origin: ['http://localhost:4200', 'https://localhost', 'http://localhost'],
   });
 
   app.setGlobalPrefix('api');
